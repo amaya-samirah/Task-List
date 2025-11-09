39145c0d41f464998f1d8f39973915bf8e447708
Amaya Shabazz <amaya.s.26292@gmail.com>
2025-11-09 18:21:03 -0500

    Connected tasks to frontend

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { onMounted, ref } from 'vue';
+import { Task } from './shared/task';
+import { remult } from 'remult';
+
+const tasks = ref<Task[]>([])
+
+const taskRepo = remult.repo(Task)
+onMounted(() => taskRepo.find().then(items => tasks.value = items))
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Todos</h1>
+  <div v-for="task in tasks">
+    <input type="checkbox" v-model="task.completed" />
+    {{ task.title }}
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
