2c69b2b3c3ea67dba2c4087b7fa689fc9b0b5546
Amaya Shabazz <amaya.s.26292@gmail.com>
2025-11-09 19:23:42 -0500

    Implemented live queries

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 import { Task } from './shared/task';
 import { remult } from 'remult';
 
 const tasks = ref<Task[]>([]);
 
 const taskRepo = remult.repo(Task);
-onMounted(() => taskRepo.find({
-  where: {
-    completed: undefined,
-  },
-}).then(items => tasks.value = items));
+onMounted(() => 
+  onUnmounted(
+    taskRepo
+      .liveQuery({
+        where: {
+          completed: undefined,
+        },
+      })
+      .subscribe((info) => (tasks.value = info.applyChanges(tasks.value)))
+  )
+);
 
 const newTaskTitle = ref("");
 
 async function addTask() {
   try{
     const newTask = await taskRepo.insert({ title: newTaskTitle.value });
-    tasks.value.push(newTask);
+    //tasks.value.push(newTask);
     newTaskTitle.value = "";
   } catch (error:any) {
     //alert(error.message);
@@ -27,7 +33,7 @@ async function addTask() {
 async function deleteTask(task: Task) {
   try {
     await taskRepo.delete(task);
-    tasks.value = tasks.value.filter((t) => t !== task);
+    //tasks.value = tasks.value.filter((t) => t !== task);
   } catch (error: any) {
     //alert(error.message);
   }
